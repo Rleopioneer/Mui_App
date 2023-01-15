1a7762d46ca971315f0f95fb2546069ce1775ea9
Ramon Leonardo <ramon.c.leo@hotmail.com>
2023-01-15 09:57:52 -0300

    Adicionando Grid em Customers

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,13 +1,28 @@
 import { useState, useEffect } from "react"
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+
 import axios from 'axios'
 
 import CustomersCard from "../components/CustomersCard"
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow:1,
+    },
+
+    card: {
+        margin: theme.spacing(4),
+
+    },
+}))
+
 const Customers = () => {
+    const classes = useStyles()
 
     const [customers, setCustomers] = useState([])
 
-    console.log(customers)
+   
 
     useEffect(()=> {
         axios.get('http://reqres.in/api/users')
@@ -21,17 +36,22 @@ const Customers = () => {
     return (
         <>
             <h1>Customers</h1>
-            {
-                customers.map(item =>(
-                    <CustomersCard
-                        name={item.first_name}
-                        lastName={item.last_name}
-                        email={item.email}
-                        avatar={item.avatar}
-                    />
-                ) )
-                
-            }
+            <Grid container>
+                {
+                    customers.map(item =>(
+                        <Grid item xs={12} md={4}>
+                            <CustomersCard
+                                name={item.first_name}
+                                lastName={item.last_name}
+                                email={item.email}
+                                avatar={item.avatar}
+                                className={classes.card}
+                            />
+                        </Grid>
+                    ) )
+                }
+            </Grid>
+                        
 
         </>
     )
